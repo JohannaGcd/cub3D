ffe57aae71eb73a2d7d066496d06a64b3a71af01
TerraBreus <studentbreus@gmail.com>
2026-01-05 11:38:54 +0100

    added image creation for the main frame
    not trying to be cool. Main frame as in the image we will constantly
    draw our frame on and the push to the window

--- src/init_data.c
+++ src/init_data.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 11:09:49 by zivanov           #+#    #+#             */
-/*   Updated: 2025/12/30 17:14:16 by zivanov          ###   ########.fr       */
+/*   Updated: 2026/01/05 11:28:47 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,34 @@
  * 		- mlx_window
  * 		- mlx pointer
  * 		- setting window size
+ *
+ * 		The `my_mlx_create_image` is somewhat vague;
+ * 			- the instance variable holds the mlx instance
+ *			- the frame variable holds the img structure
 */
 
 static int	init_mlx(t_mlx *d)
 {
 	d->instance = mlx_init();
 	if (d->instance == NULL)
+	{
+		perror("mlx_init fail\n");
 		return (-1);
+	}
 	d->win_h = WINDOWHEIGHT;
 	d->win_l = WINDOWLENGTH;
 	d->window = mlx_new_window(d->instance, d->win_l, d->win_h, "cub3d");
 	if (d->window == NULL)
 	{
+		perror("mlx_new_window failed\n");
+		mlx_destroy_display(d->instance);
+		return (-1);
+	}
+	my_mlx_create_image(d->instance, &d->frame, WINDOWLENGTH, WINDOWHEIGHT);
+	if (d->frame.instance == NULL)
+	{
+		perror("my_mlx_create_image(frame) failed\n");
+		mlx_destroy_window(d->instance, d->window);
 		mlx_destroy_display(d->instance);
 		return (-1);
 	}
@@ -47,7 +63,10 @@ static int	init_mlx(t_mlx *d)
 int	init_data(t_mlx *mlx_data, t_cub3d *cub3d)
 {
 	if (init_mlx(mlx_data) != 0)
+	{
+		perror("init_mlx failed\n");
 		return (-1);
+	}
 	(void ) cub3d;
 	return (0);
 }

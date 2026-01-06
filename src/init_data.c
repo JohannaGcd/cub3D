a73e8ecf03095979e1e4b5281d02f0ed1636a028
TerraBreus <studentbreus@gmail.com>
2026-01-06 14:39:26 +0100

    added init_textures

--- src/init_data.c
+++ src/init_data.c
@@ -52,12 +52,40 @@ static int	init_mlx(t_mlx *d)
 	return (0);
 }
 
+/*  * Loading of textures. We will specifically need .xpm files.
+ *
+ * TODO
+ * It is currently uncertain whether failure of loading a 
+ * texture is considered an error. It probably is, but we
+ * can also try to use a color if no texture exists.
+ * This means that in the rendering, we check whether the
+ * instance for texture->img exists and if not, we simply
+ * draw the side with a simple color.
+*/
+
+static int	init_textures(t_mlx *mlx, t_textures *t)
+{
+	int	i;
+
+	i = 0;
+	while (i < 4)
+	{
+		t->mlx_img[i].instance = mlx_xpm_file_to_image(
+			mlx->instance, t->path[i], &t->width[i], &t->height[i]);
+		if (t->mlx_img[i].instance == NULL)
+			perror("failure in init_textures\n");
+		i++;
+	}
+	return (0);
+}
+
 /*
  * Main initialization of all data for execution part.
  * This includes:
  * 		creation of mlx pointer.
  * 		creation of mlx_win pointer.
- * 		creation of mlx images to push to screen. //TODO
+ * 		creation of mlx images to push to screen.
+ * 		loading of textures
 */
 
 int	init_data(t_mlx *mlx_data, t_cub3d *cub3d)
@@ -67,6 +95,10 @@ int	init_data(t_mlx *mlx_data, t_cub3d *cub3d)
 		perror("init_mlx failed\n");
 		return (-1);
 	}
-	(void ) cub3d;
+	if (init_textures(mlx_data, &cub3d->texture) != 0)
+	{
+		perror("init_textures failed\n");
+		return (-1);
+	}
 	return (0);
 }

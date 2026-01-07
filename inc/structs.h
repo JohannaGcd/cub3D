efb762741d455c4397dd5c97834d9696cf679389
TerraBreus <studentbreus@gmail.com>
2026-01-07 14:31:25 +0100

    norminette fix to structs.h

--- inc/structs.h
+++ inc/structs.h
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 17:05:59 by zivanov           #+#    #+#             */
-/*   Updated: 2026/01/07 13:25:53 by zivanov          ###   ########.fr       */
+/*   Updated: 2026/01/07 13:45:32 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -159,6 +159,7 @@ typedef struct s_ray_utils
 	double	ds[2];
 	double	next[2];
 }	t_ray_utils;
+
 /*
  * -- COLOR STRUCTURE --
  *  In essence this is a simple array of three integers

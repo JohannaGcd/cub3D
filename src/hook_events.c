63cd754cd0816ce03954743d0db92fadbe072763
TerraBreus <studentbreus@gmail.com>
2026-01-09 19:17:59 +0100

    added movement events

--- src/hook_events.c
+++ src/hook_events.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 14:19:22 by zivanov           #+#    #+#             */
-/*   Updated: 2026/01/07 13:48:05 by zivanov          ###   ########.fr       */
+/*   Updated: 2026/01/09 19:13:47 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,42 @@ int	close_window(void *mlx_instance)
 	return (0);
 }
 
-/*
- * I might split the function below into another function
- * if the `if() else if()` statements become too much
- * In that case I would call:
- * void	identify_keynum(t_level *level, t_mlx *mlx_data);
- *
- * NB: ESC == 65307
- * I will create enums for this later on (TODO)
-*/
-
-int	keyboard_handler(int keynum, void *voided_cub3d)
+int	key_press_handler(int keynum, t_cub3d *cub3d)
 {
-	t_cub3d	*cub3d;
-	t_level	*level;
-	t_mlx	*mlx_data;
+	t_mlx		*mlx_data;
 
-	cub3d = (t_cub3d *) voided_cub3d;
-	level = &cub3d->level;
 	mlx_data = cub3d->mlx_data;
-	if (keynum == 65307)
+	if (keynum == KEY_ESC)
 		close_window(mlx_data->instance);
-	else
-		printf("Keynum \"%i\" has no handler\n", keynum);
+	else if (keynum == KEY_W)
+		cub3d->movement.forward = 1;
+	else if (keynum == KEY_S)
+		cub3d->movement.backward = 1;
+	else if (keynum == KEY_A)
+		cub3d->movement.left = 1;
+	else if (keynum == KEY_D)
+		cub3d->movement.right = 1;
+	else if (keynum == KEY_LEFT_ARROW)
+		cub3d->movement.rotate_left = 1;
+	else if (keynum == KEY_RIGHT_ARROW)
+		cub3d->movement.rotate_right = 1;
+	return (0);
+}
+
+int	key_release_handler(int keynum, t_cub3d *cub3d)
+{
+	if (keynum == KEY_W)
+		cub3d->movement.forward = 0;
+	else if (keynum == KEY_S)
+		cub3d->movement.backward = 0;
+	else if (keynum == KEY_A)
+		cub3d->movement.left = 0;
+	else if (keynum == KEY_D)
+		cub3d->movement.right = 0;
+	else if (keynum == KEY_LEFT_ARROW)
+		cub3d->movement.rotate_left = 0;
+	else if (keynum == KEY_RIGHT_ARROW)
+		cub3d->movement.rotate_right = 0;
 	return (0);
 }
 
@@ -48,6 +61,8 @@ void	hook_events(t_mlx *mlx_data, t_cub3d *cub3d)
 {
 	mlx_hook(mlx_data->window, ON_DESTROY, NO_EVENT_MASK,
 		close_window, mlx_data->instance);
-	mlx_hook(mlx_data->window, ON_KEYDOWN, NO_EVENT_MASK,
-		keyboard_handler, cub3d);
+	mlx_hook(mlx_data->window, ON_KEYDOWN, KEY_PRESS_MASK,
+		key_press_handler, cub3d);
+	mlx_hook(mlx_data->window, ON_KEYUP, KEY_RELEASE_MASK,
+		key_release_handler, cub3d);
 }

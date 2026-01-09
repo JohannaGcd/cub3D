78866f2c9a5f6b6401e2e91629a8a193cafe47a4
TerraBreus <studentbreus@gmail.com>
2026-01-09 17:55:03 +0100

    renamed delta_t variable in game_loop to delta_t_ms

--- src/game_loop.c
+++ src/game_loop.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 16:34:07 by zivanov           #+#    #+#             */
-/*   Updated: 2026/01/09 17:02:53 by zivanov          ###   ########.fr       */
+/*   Updated: 2026/01/09 17:30:18 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,22 @@ int	game_loop(t_cub3d *cub3d)
 {
 	static long	last_frame_time = 0;
 	long		current_frame_time;
-	long		delta_t;
+	long		delta_t_ms;
 
 	if (last_frame_time == 0)
+	{
 		last_frame_time = get_time_ms();
+		return (0);						//Technically unneccesary since we check
+	}
 	current_frame_time = get_time_ms();
-	delta_t = current_frame_time - last_frame_time;
+	delta_t_ms = current_frame_time - last_frame_time;
 	//If no time passed at all (very unlikely)
 	//we will just say 1ms has passed. I doubt 
 	//people can see this.
-	if (delta_t == 0)
+	if (delta_t_ms == 0)
 		return (0);
 	//update_player_movement(cub3d);	TODO
 	render_frame(cub3d, cub3d->mlx_data);
-	printf("delta_t %lu\n", delta_t);
 	last_frame_time = current_frame_time;
 	return (0);
 }

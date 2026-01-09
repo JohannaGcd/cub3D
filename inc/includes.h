42566e2de1583dc35ab1263fa6167133734823a2
TerraBreus <studentbreus@gmail.com>
2026-01-09 17:53:54 +0100

    added separate include header

---
+++ inc/includes.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   includes.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/09 17:49:21 by zivanov           #+#    #+#             */
+/*   Updated: 2026/01/09 17:52:46 by zivanov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Not a very interesting header but this file contains all the
+ * standard libraries that should be included on the device.
+ * The mlx library has been specifically left out since it gets downloaded
+ * by the makefile manually.
+*/
+
+#ifndef INCLUDES_H
+# define INCLUDES_H
+# include <math.h>
+# include <stdio.h>
+# include <stdbool.h>
+# include <stdlib.h>
+# include <sys/time.h>
+#endif

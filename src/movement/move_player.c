ae4459cd630cf6256c999b85befab1963a832b06
TerraBreus <studentbreus@gmail.com>
2026-01-14 13:09:49 +0100

    fixed moving bug in move_player

--- src/movement/move_player.c
+++ src/movement/move_player.c
@@ -18,13 +18,19 @@ void	print_player_dir(t_level *level, double deg_rot)
 	printf("Moving angle: %f\n", (deg_rot / M_PI) * 180); 
 }
 
+/*
+ * Note that because we consider the point (0.0) to be in the
+ * top left corner, we get a -sin() for the y dimension!
+ * This can be seen in shoot_ray.c (set_xy_pos_and_delta).
+*/
+
 void	move_player(t_level *level, double time_s, double angle_rad)
 {
 	double	new_x;
 	double	new_y;
 
 	new_x = level->player_pos_x + (cos(angle_rad) * VELOCITY * time_s);
-	new_y = level->player_pos_y +  (sin(angle_rad) * VELOCITY * time_s);
+	new_y = level->player_pos_y + -(sin(angle_rad) * VELOCITY * time_s);
 	print_player_dir(level, angle_rad);
 	if (check_collision(level, (int) new_x, (int) new_y))
 	{

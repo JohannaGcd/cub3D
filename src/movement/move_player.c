aac2e1dcbd995cb70c49ef4916a00a7a22021787
TerraBreus <studentbreus@gmail.com>
2026-01-17 15:25:02 +0100

    added better movement

--- src/movement/move_player.c
+++ src/movement/move_player.c
@@ -6,7 +6,7 @@
 /*   By: zivanov <zivanov@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/10 12:35:26 by zivanov           #+#    #+#             */
-/*   Updated: 2026/01/13 15:27:08 by zivanov          ###   ########.fr       */
+/*   Updated: 2026/01/17 15:24:05 by zivanov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,8 @@ void	move_player(t_level *level, double time_s, double angle_rad)
 
 	new_x = level->player_pos_x + (cos(angle_rad) * VELOCITY * time_s);
 	new_y = level->player_pos_y + -(sin(angle_rad) * VELOCITY * time_s);
-	if (check_collision(level, (int) new_x, (int) new_y))
-	{
-		printf("WALL COLLISIONS!\n");
-		return ;
-	}
-	level->player_pos_x = new_x;
-	level->player_pos_y = new_y;
+	if (check_collision(level, (int) new_x, (int) level->player_pos_y) != 1)
+		level->player_pos_x = new_x;
+	if (check_collision(level, (int) level->player_pos_x, (int) new_y) != 1)
+		level->player_pos_y = new_y;
 }
